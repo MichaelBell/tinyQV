8804be46261e7ad9ca9c1cd8ce04ecd5f345583c
Mike Bell <mdb036@gmail.com>
2024-02-06 21:52:17 +0000

    Fix instruction start

--- cpu/mem_ctrl.v
+++ cpu/mem_ctrl.v
@@ -52,7 +52,7 @@ module tinyqv_mem_ctrl (
         if (qspi_busy || qspi_write_done) begin
             // A transaction is running
             if (instr_active) begin
-                if (instr_fetch_restart) begin
+                if (instr_fetch_restart && !instr_fetch_started) begin
                     // Stop immediately on restart
                     stop_txn = 1;
                 end else if (qspi_data_ready && qspi_data_byte_idx == 2'b01) begin
