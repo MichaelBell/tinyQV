bb3bccf85eb17d20ec2bcbd0e78434b4c0d2e4b2
Mike Bell <mdb036@gmail.com>
2024-02-08 19:07:16 +0000

    Configurable SPI clock divider

--- peri/spi/spi.v
+++ peri/spi/spi.v
@@ -22,18 +22,25 @@ module spi_ctrl (
     input  [7:0] data_in,  // Data to transmit
     input        start,    // Signal to start a transfer, set high for 1 clock when busy is low
     output [7:0] data_out, // Data read, valid when busy is low
-    output reg   busy      // Whether a transfer is in progress
+    output reg   busy,     // Whether a transfer is in progress
+
+    // Configuration
+    input        set_config,  // Set high to change the clock divider
+    input  [1:0] divider_in   // SPI clock is input clock divided by 2 * (divider_in + 1)
 );
 
     reg [7:0] data;
     reg [2:0] bits_remaining;
     reg       end_txn_reg;
+    reg [1:0] clock_count;
+    reg [1:0] clock_divider;
 
     always @(posedge clk) begin
         if (!rstn) begin
             busy <= 0;
             spi_select <= 1;
             spi_clk_out <= 1;
+            clock_count <= 0;
         end else begin
             if (!busy) begin
                 if (start) begin
@@ -46,18 +53,30 @@ module spi_ctrl (
                     spi_clk_out <= 0;
                 end
             end else begin
-                spi_clk_out <= !spi_clk_out;
-                if (spi_clk_out) begin
-                    data <= {data[6:0], spi_miso};
-                    if (bits_remaining == 0) begin
-                        busy <= 0;
-                        spi_select <= end_txn_reg;
-                        spi_clk_out <= 1;
-                    end else begin
-                        bits_remaining <= bits_remaining - 1;
+                clock_count <= clock_count + 2'b01;
+                if (clock_count == clock_divider) begin
+                    clock_count <= 0;
+                    spi_clk_out <= !spi_clk_out;
+                    if (spi_clk_out) begin
+                        data <= {data[6:0], spi_miso};
+                        if (bits_remaining == 0) begin
+                            busy <= 0;
+                            spi_select <= end_txn_reg;
+                            spi_clk_out <= 1;
+                        end else begin
+                            bits_remaining <= bits_remaining - 1;
+                        end
                     end
                 end
             end
+        end
+    end
+
+    always @(posedge clk) begin
+        if (!rstn) begin
+            clock_divider <= 1;
+        end else if (set_config) begin
+            clock_divider <= divider_in;
         end
     end
 
