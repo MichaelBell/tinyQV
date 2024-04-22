71892caa52f55379c976174b65acc7b679feedd0
Mike Bell <mdb036@gmail.com>
2024-04-22 16:28:37 +0100

    Disable verification for now

--- verify/verify.sh
+++ verify/verify.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-./lint.sh
+#./lint.sh
 sby -d sby/qspi_ctrl -f qspi_ctrl.sby
-sby -d sby/alu_simple -f alu.sby alu
-sby -d sby/alu_shift -f alu.sby shift
+#sby -d sby/alu_simple -f alu.sby alu
+#sby -d sby/alu_shift -f alu.sby shift
 
 # Leaving multiply commented out as this takes several minutes
 #sby -d sby/alu_mul -f alu.sby mul 
